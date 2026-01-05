73642e3d3de52e68ca1d45dc467ea25c7c5bcf21
Odusanya Ademola <odusanyademola11@gmail.com>
2026-01-05 09:44:26 +0100

    Fix the post item for large devices

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,11 +1,12 @@
 <div
-  class="rounded-lg bg-white block mb-8 p-4 sm:p-6
-         mx-3 sm:mx-0
-         border border-default rounded-base shadow-xs">
+    class="rounded-lg bg-white block mb-8
+           p-4 md:p-6
+           mx-3 md:mx-0
+           border border-default rounded-base shadow-xs">
 
     <a href="#">
         <img
-            class="rounded-base w-full h-40 sm:h-48 object-cover"
+            class="rounded-base w-full h-40 md:h-auto object-cover"
             src="https://flowbite.com/docs/images/blog/image-1.jpg"
             alt=""
         />
@@ -13,14 +14,14 @@
 
     <a href="#">
         <h5
-            class="mt-4 sm:mt-6 mb-2
-                   text-lg sm:text-xl lg:text-2xl
+            class="mt-4 md:mt-6 mb-2
+                   text-lg md:text-2xl
                    font-semibold tracking-tight text-heading">
             {{ $post->title }}
         </h5>
     </a>
 
-    <p class="mb-4 sm:mb-6 text-sm sm:text-base text-body">
+    <p class="mb-4 md:mb-6 text-sm md:text-base text-body">
         {{ Str::words($post->content, 20) }}
     </p>
 
@@ -28,12 +29,15 @@
        class="text-white inline-flex items-center
               bg-blue-600 hover:bg-blue-500
               shadow-xs font-medium
-              text-xs sm:text-sm
-              px-3 py-2 sm:px-4 sm:py-2.5
+              text-xs md:text-sm
+              px-3 py-2 md:px-4 md:py-2.5
               rounded-lg focus:outline-none">
+
         Read more
+
         <svg class="w-4 h-4 ms-1.5" aria-hidden="true"
-             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+             xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round"
                   stroke-linejoin="round" stroke-width="2"
                   d="M19 12H5m14 0-4 4m4-4-4-4" />
