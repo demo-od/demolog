2084f060a833ed50e735aca3a0a92b14eabd19c4
Odusanya Ademola <odusanyademola11@gmail.com>
2026-01-05 22:23:15 +0100

    Fix the thumbnail

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -5,11 +5,8 @@
            border border-default rounded-base shadow-xs">
 
     <a href="#">
-        <img
-            class="rounded-base w-full h-40 md:h-auto object-cover"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-        />
+        <img class="rounded-base w-full aspect-[16/9] object-cover"
+            src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
     </a>
 
     <a href="#">
@@ -26,7 +23,7 @@
     </p>
 
     <a href="#"
-       class="text-white inline-flex items-center
+        class="text-white inline-flex items-center
               bg-blue-600 hover:bg-blue-500
               shadow-xs font-medium
               text-xs md:text-sm
@@ -35,12 +32,10 @@
 
         Read more
 
-        <svg class="w-4 h-4 ms-1.5" aria-hidden="true"
-             xmlns="http://www.w3.org/2000/svg"
-             fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round"
-                  stroke-linejoin="round" stroke-width="2"
-                  d="M19 12H5m14 0-4 4m4-4-4-4" />
+        <svg class="w-4 h-4 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M19 12H5m14 0-4 4m4-4-4-4" />
         </svg>
     </a>
 </div>
